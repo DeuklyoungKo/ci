eecc7b59d1975f89991d420d318b1f6e73313145
Deuklyoung Ko <deutschgon@gmail.com>
2018-08-16 04:03:30 +0000

    set design

---
+++ application/controllers/Main.php
@@ -0,0 +1,14 @@
+<?php if(!defined('BASEPATH')) exit ('No direct script access allowed');
+
+class Main extends MY_Controller{
+  function __constructor(){
+    parent::__constructor();
+  }
+
+
+  function index(){
+    $this->load->view('head');
+
+  }
+}
+?>
