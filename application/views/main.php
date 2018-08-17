3bd6d8a87f89bfa1850ee0a77a968508e96f0cf9
Deuklyoung Ko <deutschgon@gmail.com>
2018-08-17 12:48:17 +0000

    finished 'About this site' menu

--- application/views/main.php
+++ application/views/main.php
@@ -114,7 +114,7 @@
         <div class="row">
           <div class="col-lg-12 text-center">
             <h2 class="section-heading text-uppercase">Portfolio</h2>
-            <h3 class="section-subheading text-muted">representation websites still online<br>(I was involved as php developer)</h3>
+            <h3 class="section-subheading text-muted">representation websites still online<br>(I was involved as server-side developer)</h3>
           </div>
         </div>
         <div class="row">
@@ -228,7 +228,8 @@
                     <h4 class="subheading">Migam communication</h4>
                   </div>
                   <div class="timeline-body">
-                    <p class="text-muted">Web agency company, <br>Position : Assistant Manager of Web Development Team, <br> Main Task : Developing websites</p>
+                    <p class="text-muted">I made this website alone. I used a Bootstrap free theme because I am not a graphic designer.</p>
+                    <p class="text-muted">I made this website alone. I used a Bootstrap free theme because I am not a graphic designer.</p>
                   </div>
                 </div>
               </li>
@@ -296,88 +297,21 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-sm-4">
-            <div class="team-member">
-              <img class="mx-auto rounded-circle" src="./git/design/img/team/1.jpg" alt="">
-              <h4>Kay Garland</h4>
-              <p class="text-muted">Lead Designer</p>
-              <ul class="list-inline social-buttons">
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-sm-4">
-            <div class="team-member">
-              <img class="mx-auto rounded-circle" src="./git/design/img/team/2.jpg" alt="">
-              <h4>Larry Parker</h4>
-              <p class="text-muted">Lead Marketer</p>
-              <ul class="list-inline social-buttons">
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-sm-4">
-            <div class="team-member">
-              <img class="mx-auto rounded-circle" src="./git/design/img/team/3.jpg" alt="">
-              <h4>Diana Pertersen</h4>
-              <p class="text-muted">Lead Developer</p>
-              <ul class="list-inline social-buttons">
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fa fa-linkedin"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
+          <div class="col-lg-8 mx-auto text-center">
+            <img src="./static/img/dgon_eu.png" class="img-fluid mx-auto d-block" alt="Responsive image">
           </div>
         </div>
         <div class="row">
-          <div class="col-lg-8 mx-auto text-center">
-            <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+          <div class="col-lg-8 mx-auto">
+            <p class="large text-muted addTopPadding">I made this website alone, but I used a Bootstrap free theme because I am not a graphic designer.</p>
+            <p class="large text-muted addTopPadding">Github : <a href="https://github.com/DeuklyoungKo/ci" target="_blank">https://github.com/DeuklyoungKo/ci</a></p>
           </div>
         </div>
       </div>
     </section>
 
     <!-- Clients -->
+    <!--
     <section class="py-5">
       <div class="container">
         <div class="row">
@@ -404,7 +338,7 @@
         </div>
       </div>
     </section>
-
+    -->
     <!-- Contact -->
     <section id="contact">
       <div class="container">
